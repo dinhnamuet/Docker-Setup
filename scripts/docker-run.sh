06db288bffd1d0c464bcfae33ef97ca1e0149b9d
dinhnamuet <dinhnamuet@gmail.com>
2026-05-01 11:34:54 +0700

    fix: docker image: matching username uid in docker feat: docker rootfs: add host directories for caching to improve performance
    The benefit of matching the user to these UID and GID values
    is that once the docker build is done, and the user returns
    to a host shell, all files are accessible and owned by the user,
    rather than an unknown UID. This makes it much easier to modify
    configuration files and output binaries after the docker commands
    have been run. If you would like to do the same, modify the Dockerfile
    accordingly and rebuild. On the next invocation of the docker container,
    the GID and UID will match yours, and the files generated will be owned
    by your username on the host machine.
    
    The Yocto build infrastructure has smart caching of downloads and build state.
    This allows for a build to be vastly accelerated in cases where the artifacts
    have previously been built in the exact configuration required. In order to
    take advantage of this, the build infrastructure allows the user to point to
    centralized SSTATE and Download directories or servers.  In the case of local
    directories, this cache will be written to as well as read from, so it will
    increase efficiency over time.
    
    Signed-off-by: dinhnamuet <dinhnamuet@gmail.com>

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -2,15 +2,21 @@
 
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 WORKSPACE_DIR="${PROJECT_DIR}/ws"
+SSTATE_CACHE="${PROJECT_DIR}/sstate-cache"
+DOWNLOAD_DIR="${PROJECT_DIR}/downloads"
 IMAGE_NAME="yocto-intins"
 
 mkdir -p ${WORKSPACE_DIR}
+mkdir -p ${SSTATE_CACHE}
+mkdir -p ${DOWNLOAD_DIR}
 
 docker run -it --rm \
     --device=/dev/kvm:/dev/kvm \
     --device=/dev/net/tun:/dev/net/tun \
     --cap-add NET_ADMIN \
     --hostname buildserver \
+    -v "${SSTATE_CACHE}:/home/intins/sstate-cache" \
+    -v "${DOWNLOAD_DIR}:/home/intins/downloads" \
     -v "${WORKSPACE_DIR}:/home/intins/ws" \
     "${IMAGE_NAME}"
 
